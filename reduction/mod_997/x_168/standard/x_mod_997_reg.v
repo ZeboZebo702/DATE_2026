f163ce5fd0ab4be001620d73eb02e59b2e3a1a26
ZeboZebo702 <84804601+ZeboZebo702@users.noreply.github.com>
2026-04-08 22:08:55 +0100

    Add files via upload

---
+++ reduction/mod_997/x_168/standard/x_mod_997_reg.v
@@ -0,0 +1,10 @@
+
+module x_168_mod_997_reg(
+    input [168:1] X,
+    output [10:1] R
+    );
+
+
+assign R = X % 997;
+
+endmodule
---
+++ reduction/mod_997/x_270/standard/x_mod_997_reg.v
@@ -0,0 +1,10 @@
+
+module x_270_mod_997_reg(
+    input [270:1] X,
+    output [10:1] R
+    );
+
+
+assign R = X % 997;
+
+endmodule
